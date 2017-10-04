2040c567bafd9672ced393452fcbf78743e3ce64
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-10-04 17:37:21 +0200

    update

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var path = require('path')
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+const client = require('./src/routes/client')
+const stats = require('./src/routes/stats')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.use(express.static(__dirname + '/src/img'))
+app.use(express.static(__dirname + '/src/objects'))
+app.use(express.static(__dirname + '/src/js'))
+app.use(express.static(__dirname + '/node_modules'));
+
+// HOME
+
+app.get('/',function (req,res){
+  res.render('index')
+})
+
+
+// AUTRES ROUTES
+
+app.use(client)
+app.use(stats)
+
+//
+var clients = [];
+
+io.sockets.on('connect', function(client) {
+    clients.push(client);
+    client.on('disconnect', function() {
+        clients.splice(clients.indexOf(client), 1);
+    });
+});
+
--- src/routes/client.js
+++ src/routes/client.js
@@ -4,4 +4,8 @@ client.get('/client', function (req, res) {
     res.render('client');
 })
 
+client.get('/client/connect', function (req, res) {
+    res.render('client-home');
+})
+
 module.exports = client;
