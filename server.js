2d09748423110b0cb92ca03210821fdb4235c7e3
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-10-06 17:59:16 +0200

    socket io

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ var path = require('path')
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
+var bodyParser = require('body-parser')
 
 const client = require('./src/routes/client')
 const stats = require('./src/routes/stats')
 
+// CONFIG
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/src/img'))
 app.use(express.static(__dirname + '/src/objects'))
 app.use(express.static(__dirname + '/src/js'))
-app.use(express.static(__dirname + '/node_modules'));
 
 // HOME
 
@@ -27,13 +33,15 @@ app.get('/',function (req,res){
 app.use(client)
 app.use(stats)
 
-//
-var clients = [];
+var utilisateurs = [];
 
-io.sockets.on('connect', function(client) {
-    clients.push(client);
-    client.on('disconnect', function() {
-        clients.splice(clients.indexOf(client), 1);
-    });
+io.on('connection', function(socket) {  
+    socket.on('join', function(nom) {
+        utilisateurs.push(nom);
+        io.sockets.emit('rafraichirStats',utilisateurs)
+        socket.on('disconnect',function(){
+            utilisateurs.splice(utilisateurs.indexOf(nom), 1);
+            io.sockets.emit('rafraichirStats',utilisateurs)
+        })
+    }); 
 });
-
--- src/routes/client.js
+++ src/routes/client.js
@@ -1,11 +1,26 @@
 const client = require('express').Router();
+var nom = "";
 
 client.get('/client', function (req, res) {
-    res.render('client');
+    //console.log(nom)
+    if(nom != "")
+        res.render('client', {nom: nom});
+    else
+        res.redirect('/client/connect');
+        nom = "";
+})
+
+client.post('/client/connect', function (req, res) {
+    nom = req.body.nom
+    res.redirect('../client');
 })
 
 client.get('/client/connect', function (req, res) {
-    res.render('client-home');
+    res.render('client-connect');
 })
 
 module.exports = client;
+
+
+
+
