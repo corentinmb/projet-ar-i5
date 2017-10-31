c153f97fa1b458d7056028bdfec3fbbca227a29c
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-10-31 10:57:11 +0100

    maj client

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,17 +1,24 @@
 const client = require('express').Router();
 var nom = "";
+var base64Data = "";
 
 client.get('/client', function (req, res) {
     //console.log(nom)
-    if(nom != "")
+    if(nom != "" && base64Data != ""){
+        require("fs").writeFile("avatar-"+ Date.now() +".png", base64Data, 'base64', function(err) {
+            console.log(err);
+            });
         res.render('client', {nom: nom});
-    else
+    } else{
         res.redirect('/client/connect');
         nom = "";
+        base64Data = "";
+    }
 })
 
 client.post('/client/connect', function (req, res) {
     nom = req.body.nom
+    base64Data = req.body.base64.replace(/^data:image\/png;base64,/, "");
     res.redirect('../client');
 })
 
